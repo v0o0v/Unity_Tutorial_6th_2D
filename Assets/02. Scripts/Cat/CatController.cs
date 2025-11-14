4ce7ddf3f985e829d72021e3cb45d738f7f11177
김민석 <v0o0v@daum.net>
2025-11-14 10:19:57 +0900

    [고양이] - 고양이 애니 추가 - 점프 후 로테이션 적용

--- Assets/02. Scripts/Cat/CatController.cs
+++ Assets/02. Scripts/Cat/CatController.cs
@@ -4,13 +4,16 @@ public class CatController : MonoBehaviour
 {
     public float jumpPower = 5f;
     public float limitVelocity = 5f;
+    public float rotPower = 3f;
 
     private Rigidbody2D catRb;
     private int jumpCount;
+    private Animator catAnim;
 
     private void Start()
     {
         catRb = GetComponent<Rigidbody2D>();
+        catAnim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -22,16 +25,23 @@ public class CatController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) && jumpCount < 3)
         {
+            catAnim.SetBool("IsGround", false);
+            catAnim.SetTrigger("Jump");
             catRb.AddForceY(jumpPower, ForceMode2D.Impulse);
             jumpCount++;
             if (catRb.linearVelocityY >= limitVelocity)
                 catRb.linearVelocityY = limitVelocity;
         }
+        
+        Vector3 catRatation = transform.eulerAngles;
+        catRatation.z = catRb.linearVelocityY * rotPower;
+        transform.eulerAngles = catRatation;
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Ground"))
             jumpCount = 0;
+        catAnim.SetBool("IsGround", true);
     }
 }
